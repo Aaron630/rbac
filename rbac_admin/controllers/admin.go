6664fae4566c5457c3893be920704b42d52edd8a
dave <dave.wu@newType.io>
2020-06-01 18:29:41 +0800

    test

--- rbac_admin/controllers/admin.go
+++ rbac_admin/controllers/admin.go
@@ -1,14 +1,14 @@
 package controllers
 
 import (
+	"crypto/md5"
+	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"rbac_admin/models"
 	"strings"
 	"time"
-
-	"crypto/md5"
 
 	"github.com/astaxie/beego"
 	"github.com/dgrijalva/jwt-go"
@@ -49,16 +49,37 @@ func (c *AdminController) Post() {
 		// 密码错误
 	}
 	// 颁发一个有限期72小时的证书
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user": adminUser.Account,
-		"exp":  time.Now().Add(time.Hour * time.Duration(72)).Unix(),
-		"iat":  time.Now().Unix(),
-	})
+	type CustomeClaims struct {
+		user string
+		iat  int64
+		jwt.StandardClaims
+	}
+	customeClaims := &CustomeClaims{
+		user: adminUser.Account,
+		iat:  time.Now().Unix(),
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Second * 10).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, customeClaims)
 	tokenString, err := token.SignedString([]byte("ppp"))
 	if err != nil {
 		fmt.Println(err)
 	}
-	// json webtoken 解析
-
 	fmt.Println(tokenString)
+	// json webtoken 解析
+	tt, err := jwt.ParseWithClaims(tokenString, &CustomeClaims{}, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		// fmt.Println(((*token).Claims))
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("ppp"), nil
+	})
+	b := make([]byte, 2)
+	rand.Read(b)
+	fmt.Println(hex.EncodeToString(passwdMD5[:]))
+	fmt.Println("aaaaaa:", tt)
 }
